b02ebcb5928dbbfe03a4225f97961ca97c692d3a
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:37 -0500

    xml commenting

--- src/Soenneker.ProductBoard.HttpClients/ProductBoardOpenApiHttpClient.cs
+++ src/Soenneker.ProductBoard.HttpClients/ProductBoardOpenApiHttpClient.cs
@@ -45,11 +45,18 @@ public sealed class ProductBoardOpenApiHttpClient : IProductBoardOpenApiHttpClie
         }, cancellationToken);
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(nameof(ProductBoardOpenApiHttpClient));
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(nameof(ProductBoardOpenApiHttpClient));
